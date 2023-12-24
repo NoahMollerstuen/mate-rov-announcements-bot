9b4d7b2fc915f44bbb25565d9df89c72bb8a6f6c
Noah Mollerstuen <35637345+NoahMollerstuen@users.noreply.github.com>
2023-12-24 01:31:30 -0500

    Add support for official rulings page

--- web.py
+++ web.py
@@ -1,3 +1,4 @@
+import dataclasses
 import typing as t
 import aiohttp
 from bs4 import BeautifulSoup
@@ -5,21 +6,27 @@ from bs4 import BeautifulSoup
 import db
 
 
+MATE_FILTER_ID = "main-content"
+
+
+@dataclasses.dataclass
 class Page:
-    def __init__(self, name, url, description):
-        self.name = name
-        self.url = url
-        self.description = description
+    name: str
+    url: str
+    filter_id: str
+    description: str
 
 
 PAGES = [
-    Page("explorer", "https://materovcompetition.org/explorer", "the Explorer class specs"),
-    Page("pioneer", "https://materovcompetition.org/pioneer", "the Pioneer class specs"),
-    Page("ranger", "https://materovcompetition.org/ranger", "the Ranger class specs"),
-    Page("navigator", "https://materovcompetition.org/navigator", "the Navigator class specs"),
-    Page("scout", "https://materovcompetition.org/scout", "the Scout class specs"),
-    Page("scoring", "https://materovcompetition.org/scoring", "scoring rules"),
-    Page("worlds", "https://materovcompetition.org/world-championship", "the world championships"),
+    Page("explorer", "https://materovcompetition.org/explorer", MATE_FILTER_ID, "the Explorer class specs"),
+    Page("pioneer", "https://materovcompetition.org/pioneer", MATE_FILTER_ID, "the Pioneer class specs"),
+    Page("ranger", "https://materovcompetition.org/ranger", MATE_FILTER_ID, "the Ranger class specs"),
+    Page("navigator", "https://materovcompetition.org/navigator", MATE_FILTER_ID, "the Navigator class specs"),
+    Page("scout", "https://materovcompetition.org/scout", MATE_FILTER_ID, "the Scout class specs"),
+    Page("scoring", "https://materovcompetition.org/scoring", MATE_FILTER_ID, "scoring rules"),
+    Page("worlds", "https://materovcompetition.org/world-championship", MATE_FILTER_ID, "the world championships"),
+    Page("rulings", "https://docs.google.com/document/d/e/2PACX-1vS-i5t8yrwIYMjHzpS6sSYyuG8_quCGhyDMnxPqG2eDmI6QacK08fTVS2_VQF-d1vfjA7ydJgbu4itI/pub", "contents", "official rulings"),
+    # Page("debug", "https://c.xkcd.com/random/comic/", "comic", "debug page"),
 ]
 
 PAGES_BY_NAME = {
@@ -35,7 +42,7 @@ async def get_page_update(page: Page, session: aiohttp.ClientSession) -> t.Optio
         return None
 
     soup = BeautifulSoup(new_text_raw, 'html.parser')
-    new_text = soup.find(id="main-content").prettify()
+    new_text = soup.find(id=page.filter_id).prettify()
 
     if not await db.check_for_page(page.url):
         await db.add_page(page.url, new_text)
