eddf2809228dfdbd4f0bef15096274c3a34d8936
Noah Mollerstuen <35637345+NoahMollerstuen@users.noreply.github.com>
2023-12-24 02:32:38 -0500

    Add text only comparison mode

--- web.py
+++ web.py
@@ -1,4 +1,5 @@
 import dataclasses
+import enum
 import typing as t
 import aiohttp
 from bs4 import BeautifulSoup
@@ -9,24 +10,41 @@ import db
 MATE_FILTER_ID = "main-content"
 
 
+class ParseType(enum.Enum):
+    FULL_HTML = ("html", lambda s: s.prettify())
+    TEXT_ONLY = ("text", lambda s: s.get_text("\n"))
+
+    def __new__(cls, value, parse_func):
+        obj = object.__new__(cls)
+        obj._value_ = value
+        obj.parse_func = parse_func
+        return obj
+
+
+@dataclasses.dataclass
+class ParseOptions:
+    filter_id: t.Optional[str]
+    parse_type: ParseType
+
+
 @dataclasses.dataclass
 class Page:
     name: str
     url: str
-    filter_id: str
     description: str
+    parse_options: ParseOptions = ParseOptions(MATE_FILTER_ID, ParseType.FULL_HTML)
 
 
 PAGES = [
-    Page("explorer", "https://materovcompetition.org/explorer", MATE_FILTER_ID, "the Explorer class specs"),
-    Page("pioneer", "https://materovcompetition.org/pioneer", MATE_FILTER_ID, "the Pioneer class specs"),
-    Page("ranger", "https://materovcompetition.org/ranger", MATE_FILTER_ID, "the Ranger class specs"),
-    Page("navigator", "https://materovcompetition.org/navigator", MATE_FILTER_ID, "the Navigator class specs"),
-    Page("scout", "https://materovcompetition.org/scout", MATE_FILTER_ID, "the Scout class specs"),
-    Page("scoring", "https://materovcompetition.org/scoring", MATE_FILTER_ID, "scoring rules"),
-    Page("worlds", "https://materovcompetition.org/world-championship", MATE_FILTER_ID, "the world championships"),
-    Page("rulings", "https://docs.google.com/document/d/e/2PACX-1vS-i5t8yrwIYMjHzpS6sSYyuG8_quCGhyDMnxPqG2eDmI6QacK08fTVS2_VQF-d1vfjA7ydJgbu4itI/pub", "contents", "official rulings"),
-    # Page("debug", "https://c.xkcd.com/random/comic/", "comic", "debug page"),
+    Page("explorer", "https://materovcompetition.org/explorer", "the Explorer class specs"),
+    Page("pioneer", "https://materovcompetition.org/pioneer", "the Pioneer class specs"),
+    Page("ranger", "https://materovcompetition.org/ranger", "the Ranger class specs"),
+    Page("navigator", "https://materovcompetition.org/navigator", "the Navigator class specs"),
+    Page("scout", "https://materovcompetition.org/scout", "the Scout class specs"),
+    Page("scoring", "https://materovcompetition.org/scoring", "scoring rules"),
+    Page("worlds", "https://materovcompetition.org/world-championship", "the world championships"),
+    Page("rulings", "https://docs.google.com/document/d/e/2PACX-1vS-i5t8yrwIYMjHzpS6sSYyuG8_quCGhyDMnxPqG2eDmI6QacK08fTVS2_VQF-d1vfjA7ydJgbu4itI/pub",
+         "official rulings", ParseOptions("contents", ParseType.TEXT_ONLY)),
 ]
 
 PAGES_BY_NAME = {
@@ -42,7 +60,9 @@ async def get_page_update(page: Page, session: aiohttp.ClientSession) -> t.Optio
         return None
 
     soup = BeautifulSoup(new_text_raw, 'html.parser')
-    new_text = soup.find(id=page.filter_id).prettify()
+    if page.parse_options.filter_id is not None:
+        soup = soup.find(id=page.parse_options.filter_id)
+    new_text = page.parse_options.parse_type.parse_func(soup)
 
     if not await db.check_for_page(page.url):
         await db.add_page(page.url, new_text)
