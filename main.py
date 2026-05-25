909ce5a87728e0877127ae0038798778b28822c0
Noah Mollerstuen <noah@mollerstuen.com>
2026-05-25 12:33:06 -0500

    Fix link filtering

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from discord.ext import tasks
 import io
 import itertools
 import json
+import html
 import ghdiff
 import imgkit
 import logging
@@ -199,7 +200,7 @@ async def fetch_updates():
         new_links = []
         for link in links:
             href = link.get("href")
-            if href is not None and "http" in href and href not in old_text and link.get_text():
+            if href is not None and "http" in href and href not in old_text and html.encode(href) not in old_text and link.get_text():
                 new_links.append(link)
 
         if len(new_links) > 0:
